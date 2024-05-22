261b49bbe5ef637bb76b0eac581f15d6fa92848e
Sudhanshu Gaikwad <140872777+sudhanshu1919@users.noreply.github.com>
2024-05-22 07:08:37 +0530

    Update script.js
    Change Js Logics in thr file

--- script.js
+++ script.js
@@ -1,58 +1,42 @@
-// // Fetch data from the JSON file
+// Fetch data from the JSON file
 fetch("https://sudhanshu1313.github.io/taskjson/Data.json")
   .then((response) => response.json())
   .then((AllData) => {
     console.log(AllData);
 
-    let cartItems = []; // Initialize an array to store cart items
-
-    // Map has call Alldata from the  url and generate HTML for each item
+    // Map has call Alldata from the url and generate HTML for each item
     let Data2 = AllData.map((value, index) => {
       return `
-
-
-
       <div>
         <div class="smallbox" onclick="showbig(${index})">
-            <div class="smallleft">
+          <div class="smallleft">
+            <ul>
+              <li class="checked">
+                <div>
+                  <h2>${value.name} <span class="badget">${value.discount}</span></h2>
+                  <p>Standard Price</p>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div class="smallright">
+            <h2>${value.prise1}</h2>
+            <p>${value.prise2}</p>
+          </div>
+        </div>
+        <div class="big">
+          <div class="mostpop">most popular</div>
+          <div class="pricesection">
+            <div class="smallleft bigleft">
               <ul>
                 <li class="checked">
                   <div>
-                    <h2>${value.name} <span class="badget">${
-        value.discount
-      }</span></h2>
+                    <h2>${value.name} <span class="badget">${value.discount}</span></h2>
                     <p>Standard Price</p>
                   </div>
                 </li>
               </ul>
             </div>
-            <div class="smallright">
-              <h2>${value.prise1}</h2>
-              <p>${value.prise2}</p>
-            </div>
-        </div>
-
-
-
-          
-
-          <div class="big">
-          <div class="mostpop">most popular</div>
-          <div class="pricesection">
-       
-            <div class="smallleft bigleft">
-            <ul>
-            <li class="checked">
-              <div>
-                <h2>${value.name} <span class="badget">${
-        value.discount
-      }</span></h2>
-                <p>Standard Price</p>
-              </div>
-            </li>
-          </ul>
-            </div>
-            
             <div class="smallright">
               <h2>${value.prise1}</h2>
               <p>${value.prise2}</p>
@@ -65,45 +49,37 @@ fetch("https://sudhanshu1313.github.io/taskjson/Data.json")
               <div class="colname">Colour</div>
             </div>
             ${colorsection(index)}
-
-         
-    
-
           </div>
         </div>
-          </div>
-
-
-      `;
+      </div>`;
     }).join("");
 
-    // Display the generated HTML in the "SmallBox" element
+    // Display the generated HTML in the "cards" element
     document.getElementById("cards").innerHTML = Data2;
   })
   .catch((error) => console.error("Unable to fetch data:", error));
 
 function colorsection(index) {
   let colorelements = "";
-  for (i = 0; i <= index; i++) {
-    colorelements += ` 
+  for (let i = 0; i <= index; i++) {
+    colorelements += `
     <div class="colorsecno colorrow">
-    <div class="">#${i + 1}</div>
-    <div>
-      <select class="MainOption">
-        <option>&nbsp;S</option>
-        <option>&nbsp;M</option>
-        <option>&nbsp;L</option>
-      </select>
-    </div>
-    <div>
-      <select class="MainOption">
-        <option>&nbsp;Red</option>
-        <option>&nbsp;Blue</option>
-      </select>
-    </div>
-  </div>`;
+      <div>#${i + 1}</div>
+      <div>
+        <select class="MainOption">
+          <option>&nbsp;S</option>
+          <option>&nbsp;M</option>
+          <option>&nbsp;L</option>
+        </select>
+      </div>
+      <div>
+        <select class="MainOption">
+          <option>&nbsp;Red</option>
+          <option>&nbsp;Blue</option>
+        </select>
+      </div>
+    </div>`;
   }
-
   return colorelements;
 }
 
@@ -111,8 +87,7 @@ let prevCard = -1;
 function showbig(ind) {
   if (prevCard != -1) {
     document.getElementsByClassName("big")[prevCard].style.display = "none";
-    document.getElementsByClassName("smallbox")[prevCard].style.display =
-      "flex";
+    document.getElementsByClassName("smallbox")[prevCard].style.display = "flex";
   }
   prevCard = ind;
   document.getElementsByClassName("big")[ind].style.display = "block";
